c4a78c94fd0318aed9388588878022500b1b53da
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-17 15:44:01 +0100

    add-array-from-api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,46 @@
 <script>
+import axios from "axios";
+import { store } from "../data/store.js";
+
 export default {
   name: "AppMain",
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getData() {
+      axios
+        .get(store.apiUrl)
+        .then((result) => {
+          store.bbData = result.data;
+        })
+        .catch((error) => {
+          console.log("ERROR!");
+        });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
 };
 </script>
 
 <template>
-  <div class="container">MAIN</div>
+  <main>
+    <div class="container">
+      <div class="row">
+        <select class="form-select w-25" aria-label="Default select example">
+          <option value="1">BrBa World</option>
+          <option value="2">Breaking Bad</option>
+          <option value="3">Better Call Saul</option>
+        </select>
+      </div>
+
+      <div class="row mt-5"></div>
+    </div>
+  </main>
 </template>
 
 <style></style>
---
+++ src/data/store.js
@@ -0,0 +1,6 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    bbData: [],
+    apiUrl: "https://www.breakingbadapi.com/api/characters"
+});
